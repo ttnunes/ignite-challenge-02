c6160725de732c37f8f0eef7cf890d29ba50a5fa
Thiago Teixeira Nunes <thiagonunes.cx@gmail.com>
2021-04-09 13:08:34 -0300

    Refactoring content component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { useEffect, useState } from 'react';
 
-import { MovieCard } from './components/MovieCard';
-
 import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
 import './styles/global.scss';
-
-import './styles/content.scss';
 
 export interface GenreResponseProps {
   id: number;
@@ -17,7 +13,7 @@ export interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
+export interface MovieProps {
   imdbID: string;
   Title: string;
   Poster: string;
@@ -64,19 +60,11 @@ export function App() {
         selectedGenreId={selectedGenreId}
       />
 
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content 
+        movies={movies}
+        selectedGenre={selectedGenre}
+      />
+      
     </div>
   )
 }
